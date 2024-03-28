3e78afc7b73ab0bb1a5a2de9324a42a46b4b20b4
Ringor123 <ringor123@gmail.com>
2024-03-28 17:07:43 +0100

    changes in backend exercise 3.10

--- part3/index.js
+++ part3/index.js
@@ -4,7 +4,7 @@ const app = express()
 const cors = require('cors')
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
