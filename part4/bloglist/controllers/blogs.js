ef40c7c07f1b667be34b66fabfc447b856a98671
Ringor123 <ringor123@gmail.com>
2024-04-23 10:57:30 +0200

    exercise 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,19 +1,34 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-   const blog = new Blog(request.body)
+  const body = request.body
+  
+  const allUsers = await User.find({})
+  const userId = allUsers[0].id
+  const user = await User.findById(userId)
+  
+  const blog = new Blog({
+    title: body.title,
+    url: body.url,
+    author: body.author,
+    user: user.id
+  })
 
    if (blog.likes === undefined) {
       blog.likes = 0
    }
 
    const savedBlog = await blog.save()
+   user.blogs = user.blogs.concat(savedBlog._id)
+   await user.save()
+
    if (savedBlog) {
     response.status(201).json(savedBlog)
    } else {
